6ef450693ebc92e85bb5315b85cef24e24331a5d
Daniel Fischer <dfischer17htl@gmail.com>
2021-07-28 14:13:52 +0200

    Added dark-mode toggle

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -16,8 +16,12 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
     this.authService = this.pAuthService;
+    this.addPrefersColorSchemeListener();
   }
 
+  /*
+  Prüft Login-Daten mit authService => wenn erfolgreich weiterleitung auf app
+  */
   login() {
     console.log('User: ' + this.usrName + ' Password ' + this.pwd);
 
@@ -25,5 +29,39 @@ export class LoginPage implements OnInit {
       console.log('Logged in!');
     }
     this.router.navigate(['/app']); // gehört in if für authenication
+  }
+
+  /*
+  Reagiert auf Themeaenderung im System
+  */
+  addPrefersColorSchemeListener() {
+    const colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const setColorScheme = e => {
+      if (e.matches) {
+        // Dark
+        console.log('Detected Dark');
+        this.enableDarkMode(true);
+      } else {
+        // Light
+        console.log('Detected Light');
+        this.enableDarkMode(false);
+      }
+    };
+
+    setColorScheme(colorSchemeQueryList);
+    colorSchemeQueryList.addListener(setColorScheme);
+  }
+
+  /*
+  Schaltet Dark-Mode ein/aus
+  */
+  enableDarkMode(dark: boolean) {
+    if (dark) {
+      document.body.classList.add('dark');
+    }
+    else {
+      document.body.classList.remove('dark');
+    }
   }
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  isDark: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.initDarkModeToggle();
   }
 
+  /*
+  Callback für dark-mode ion-toggle
+  */
+  toggleDarkMode() {
+    this.enableDarkMode(this.isDark);
+  }
+
+  /*
+  Schaltet Dark-Mode ein/aus
+  */
+  enableDarkMode(dark: boolean) {
+    if (dark) {
+      console.log('Enabled dark-mode');
+      document.body.classList.add('dark');
+    }
+    else {
+      console.log('Disabled dark-mode');
+      document.body.classList.remove('dark');
+    }
+
+    this.isDark = document.body.classList.contains('dark');
+  }
+
+  /*
+  Synchronisiert den Zustand des DarkMode ion-toggle (ein/aus) mit den Systemeinstellungen und
+  innerhalb der Anwendung
+  */
+  initDarkModeToggle() {
+    console.log('Init dark-mode toggle ', document.body.classList.contains('dark'));
+    this.isDark = document.body.classList.contains('dark');
+  }
 }
